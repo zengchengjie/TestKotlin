603cb7ac59d061664b940b1ae619afb2b7823149
zengchengjie <zengchengjieit@163.com>
2017-12-27 09:09:36 +0800

    kotlin练习

---
+++ app/src/main/java/com/example/zcj/testkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zcj.testkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
